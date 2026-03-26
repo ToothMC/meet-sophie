9a5582771402ad2ae2513833db02d8065c194b9f
ToothMC <michael@mmhammer.org>
2026-03-26 22:28:42 +0200

    auto: save [22:28]

--- api/session.js
+++ api/session.js
@@ -506,9 +506,9 @@ export default async function handler(req, res) {
         input_audio_format: "pcm16",
         turn_detection: {
           type: "server_vad",
-          threshold: 0.5,
+          threshold: 0.6,
           prefix_padding_ms: 300,
-          silence_duration_ms: 400,
+          silence_duration_ms: 800,
           idle_timeout_ms: null,
           create_response: false,
           interrupt_response: true,
