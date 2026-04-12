eef47b3194a3a210197ffdc3da49836922eb64c8
ToothMC <michael@mmhammer.org>
2026-04-12 17:49:07 +0300

    auto: save [17:49]

--- api/meeting.js
+++ api/meeting.js
@@ -15,6 +15,7 @@ import { TOKEN_COSTS } from "../lib/billing-constants.js";
 import { trackCost } from "../lib/ai/cost-tracker.js";
 import { getWeather, webSearch, getNews, getWikipedia, getFlightStatus, getAirportFlights } from "./ai/tools.js";
 import mammoth from "mammoth";
+import pdfParse from "pdf-parse/lib/pdf-parse.js";
 
 // ---------------------------------------------------------------------------
 // Auth helpers (shared with chat.js)
